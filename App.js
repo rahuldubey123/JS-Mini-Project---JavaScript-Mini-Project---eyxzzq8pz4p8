35e0aee4a371217d04bbe4f7c3a0aae679518620
rahuldubey123 <99980292+rahuldubey123@users.noreply.github.com>
2022-07-22 17:43:50 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-
+ function login()
+         {
+    var uname=document.getElementById('username').value;
+    var pass=document.getElementById('password').value;
+    if(uname=="world" && pass=="123")
+    {
+        location.assign("file:///C:/Users/Kanta/Desktop/page.html")
+    }
+    else
+    {
+        window.alert("login failed")
+    }
+}
