7b5c227da842ff5f06fcf91d2d7647e7a89cfc90
rahuldubey123 <99980292+rahuldubey123@users.noreply.github.com>
2022-07-23 13:18:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
- function login()
-         {
-    var uname=document.getElementById('username').value;
-    var pass=document.getElementById('password').value;
-    if(uname=="world" && pass=="123")
-    {
-        location.assign("page.html")
-    }
-    else
-    {
-        window.alert("login failed")
-    }
-}
+const btns = document.querySelectorAll('.btn-container')
+const submitBtn = document.querySelector('.submit-btn')
+const span = document.querySelector('.card-2 .card-body span')
+const initialCard = document.querySelector('.card-1')
+// i gave the first card a class of .card-1
+const SecondCard = document.querySelector('.card-2')
+Array.from(btns).forEach(btn =>{
+btn.addEventListener('click', e => {
+  let text = e.target.textContent
+  submitBtn.addEventListener('click', e =>{
+    initialCard.classList.add('hide')
+    SecondCard.classList.remove('hide')
+    span.textContent = text
+  })
+})
+})
